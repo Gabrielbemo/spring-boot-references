a3e4e41d38271cf7aba3f0e0a4073825f83a6091
Gabrielbemo <gabrielbemoura@gmail.com>
2021-06-20 17:19:06 -0300

    Add dependency injection, lombok and devtools

--- src/main/java/br/com/springreferences/controller/GameController.java
+++ src/main/java/br/com/springreferences/controller/GameController.java
@@ -1,17 +1,28 @@
 package br.com.springreferences.controller;
 
 import br.com.springreferences.domain.Game;
+import br.com.springreferences.util.DateUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("game")
+@RequestMapping("/game")
+@Log4j2
+@AllArgsConstructor
 public class GameController {
-    @GetMapping(path = "list")
+
+    private DateUtil dateUtil;
+
+    @GetMapping(path = "/list")
     public List<Game> list() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Game("minecraft"), new Game("DOS2"));
     }
 }
